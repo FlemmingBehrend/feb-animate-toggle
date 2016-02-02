8bc30b8852d5e2020b9ec68e2d3ba84b9ad1cef1
FlemmingBehrend <topfeb@gmail.com>
2016-02-02 22:49:03 +0100

    minimum starting code

--- animate-toggle.js
+++ animate-toggle.js
@@ -1,18 +1,56 @@
 (function() {
     'use strict';
 
-    angular.module('feb-animate-toggle', []).directive('febAnimateToggle', AnimateToggle);
+    angular.module('feb.animate-toggle', []).directive('febAnimateToggle', AnimateToggle);
 
     function AnimateToggle() {
 
         var directive = {
+            controller: Ctrl,
+            controllerAs: 'vm',
             restrict: 'A',
-            link: link
+            link: link,
+            scope: {
+                control: '='
+            },
+            transclude: true,
+            template:
+                '<div>' +
+                    '<div ng-click="vm.toggle()">{{vm.control.toggleText}}</div>' +
+                    '<div ng-transclude class="{{vm.toggleId}}" style="display: {{vm.display}}"></div>' +
+                '</div>'
         };
 
         return directive;
 
-        function link(scope, element, attrs) {
+        function link(scope, element, attrs, controllers) {
+            controllers.control = scope.control;
+            controllers.toggleId = attrs.febAnimateToggle;
+            controllers.init();
+        }
+
+        function Ctrl() {
+            var vm = this;
+
+            vm.toggle = toggle;
+            vm.init = initialize;
+
+            function initialize() {
+                vm.display = vm.control.show ? 'block' : 'none';
+            }
+
+            function animate() {
+                if (vm.control.show) {
+                    $('.' + vm.toggleId).stop(true, true).slideDown(vm.control.duration);
+                } else {
+                    $('.' + vm.toggleId).stop(true, true).slideUp(vm.control.duration);
+                }
+            }
+
+            function toggle() {
+                vm.control.show = !vm.control.show;
+                animate();
+            }
 
         }
 
